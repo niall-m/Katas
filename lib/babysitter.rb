5248afe87be47324c0b92e43d43d6d19993f89c1
Niall Mahford <niall.mahford@gmail.com>
2018-12-09 17:53:31 -0500

    Configure and test babysitter start time.

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -1,3 +1,7 @@
 class Babysitter
-  
+  attr_reader :start_time
+
+  def initialize(start_time) 
+    @start_time = start_time
+  end
 end
--- spec/babysitter_spec.rb
+++ spec/babysitter_spec.rb
@@ -3,7 +3,7 @@ require 'babysitter'
 describe Babysitter do 
   context '#initialize' do 
     it 'has a start time' do 
-      
+      expect(Babysitter.new(17).start_time).to eq(17)
     end
   end
 end
