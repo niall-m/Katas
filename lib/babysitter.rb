5374e66e853bed7e51273b9c700d082548820f75
Niall Mahford <niall.mahford@gmail.com>
2018-12-10 11:59:51 -0500

    Refactor client attribute extract to include rates.

--- lib/babysitter.rb
+++ lib/babysitter.rb
@@ -1,12 +1,12 @@
 require 'job' 
 
 class Babysitter
-  attr_reader :start_time, :end_time, :client
+  attr_reader :start_time, :end_time, :client, :rates
 
   def initialize(start_time, end_time, client) 
     Job.valid_hours?(start_time, end_time) && Job.valid_client?(client)
     
     @start_time, @end_time = start_time, end_time
-    @client = Job.clients.select { |key, value| key == client }.keys[0]
+    @client, @rates = Job.clients.find { |family, values| family == client }
   end
 end
